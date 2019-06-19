c5e254c1ebc8e2f3a96c359235bce2f7b95f7b60
Michael Santos <michael.shel96@gmail.com>
2019-06-19 16:34:57 -0300

    exercício 5.4 - implementando a seleção de lugares, ingressos e tipo de ingressos

--- src/main/java/br/com/caelum/ingresso/model/Sessao.java
+++ src/main/java/br/com/caelum/ingresso/model/Sessao.java
@@ -3,14 +3,18 @@ package br.com.caelum.ingresso.model;
 import java.math.BigDecimal;
 import java.math.RoundingMode;
 import java.time.LocalTime;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 
 @Entity
 public class Sessao {
@@ -24,26 +28,32 @@ public class Sessao {
 	@ManyToOne
 	private Filme filme;
 	private BigDecimal preco;
-	
+	@OneToMany(mappedBy = "sessao", fetch = FetchType.EAGER)
+	private Set<Ingresso> ingressos = new HashSet<>();
 
 	@Deprecated
-	public Sessao() {}
-	
+	public Sessao() {
+	}
+
 	public Sessao(LocalTime horario, Sala sala, Filme filme) {
 		this.horario = horario;
 		this.sala = sala;
 		this.filme = filme;
 		this.preco = sala.getPreco().add(filme.getPreco());
 	}
-	
+
+	public boolean isDisponivel(Lugar lugarSelecionado) {
+		return ingressos.stream().map(Ingresso::getLugar).noneMatch(lugar -> lugar.equals(lugarSelecionado));
+	}
+
 	public Map<String, List<Lugar>> getMapaDeLugares() {
 		return sala.getMapaDeLugares();
 	}
-	
+
 	public BigDecimal getPreco() {
 		return preco.setScale(2, RoundingMode.HALF_UP);
 	}
-	
+
 	public void setPreco(BigDecimal preco) {
 		this.preco = preco;
 	}
@@ -74,6 +84,14 @@ public class Sessao {
 
 	public Integer getId() {
 		return id;
+	}
+
+	public Set<Ingresso> getIngresso() {
+		return ingressos;
+	}
+
+	public void setIngresso(Set<Ingresso> ingresso) {
+		this.ingressos = ingresso;
 	}
 
 }
--- src/test/java/br/com/caelum/ingresso/model/descontos/DescontoTest.java
+++ src/test/java/br/com/caelum/ingresso/model/descontos/DescontoTest.java
@@ -10,31 +10,35 @@ import org.junit.Test;
 
 import br.com.caelum.ingresso.model.Filme;
 import br.com.caelum.ingresso.model.Ingresso;
+import br.com.caelum.ingresso.model.Lugar;
 import br.com.caelum.ingresso.model.Sala;
 import br.com.caelum.ingresso.model.Sessao;
+import br.com.caelum.ingresso.model.TipoDeIngresso;
 
 public class DescontoTest {
 
 	private Sessao sessao;
+	private Lugar lugar;
 	
 	@Before
 	public void preparaDesconto() {
+		lugar = new Lugar("1",1);
 		Sala sala = new Sala("Eldorado - IMAX", new BigDecimal("20.5"));
 		Filme filme = new Filme("Rogue One", Duration.ofMinutes(120), "SCI-FI", new BigDecimal("12"));
-		this.sessao = new Sessao(LocalTime.parse("10:00:00"), sala, filme);
+		sessao = new Sessao(LocalTime.parse("10:00:00"), sala, filme);
 	}
 
 	@Test
 	public void naoDeveConcederDescontoParaIngressoNormal() {
 
-		Ingresso ingresso = new Ingresso(sessao, new SemDesconto());
+		Ingresso ingresso = new Ingresso(sessao, TipoDeIngresso.INTEIRO, lugar);
 		BigDecimal precoEsperado = new BigDecimal("32.50");
 		Assert.assertEquals(precoEsperado, ingresso.getPreco());
 	}
 
 	@Test
 	public void deveConcederDescontoDe30PorcentoParaIngressosDeclientesDeBancos() {
-		Ingresso ingresso = new Ingresso(sessao, new DescontoParaBancos());
+		Ingresso ingresso = new Ingresso(sessao, TipoDeIngresso.BANCO, lugar);
 		BigDecimal precoEsperado = new BigDecimal("22.75");
 		Assert.assertEquals(precoEsperado, ingresso.getPreco());
 	}
@@ -42,7 +46,7 @@ public class DescontoTest {
 	@Test
 	public void deveConcederDescontoDe50PorcentoParaIngressosDeEstudantes() {
 
-		Ingresso ingresso = new Ingresso(sessao, new DescontoParaEstudantes());
+		Ingresso ingresso = new Ingresso(sessao, TipoDeIngresso.ESTUDANTE, lugar);
 		BigDecimal precoEsperado = new BigDecimal("16.25");
 		Assert.assertEquals(precoEsperado, ingresso.getPreco());
 	}
