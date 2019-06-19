94065bc87ac47004ff1072e3835c0da81c0c3954
Michael Santos <michael.shel96@gmail.com>
2019-06-19 12:39:48 -0300

    Exercício 4.4 - consumindo serviço para detalhes do filme

--- src/main/java/br/com/caelum/ingresso/controller/FilmeController.java
+++ src/main/java/br/com/caelum/ingresso/controller/FilmeController.java
@@ -18,8 +18,10 @@ import org.springframework.web.servlet.ModelAndView;
 
 import br.com.caelum.ingresso.dao.FilmeDao;
 import br.com.caelum.ingresso.dao.SessaoDao;
+import br.com.caelum.ingresso.model.DetalhesDoFilme;
 import br.com.caelum.ingresso.model.Filme;
 import br.com.caelum.ingresso.model.Sessao;
+import br.com.caelum.ingresso.rest.OmdbClient;
 
 /**
  * Created by nando on 03/03/17.
@@ -32,6 +34,8 @@ public class FilmeController {
     private FilmeDao filmeDao;
     @Autowired
     private SessaoDao sessaoDao;
+    @Autowired
+    private OmdbClient client;
 
     @GetMapping({"/admin/filme", "/admin/filme/{id}"})
     public ModelAndView form(@PathVariable("id") Optional<Integer> id, Filme filme){
@@ -94,6 +98,9 @@ public class FilmeController {
     	Filme filme = filmeDao.findOne(id);
     	List<Sessao> sessoes = sessaoDao.buscaSessoesDoFilme(filme);
     	
+    	Optional<DetalhesDoFilme> detalhesDoFilme = client.request(filme);
+    	
+    	modelAndView.addObject("detalhes", detalhesDoFilme.orElse(new DetalhesDoFilme()));
     	modelAndView.addObject("sessoes", sessoes);    	
     	
     	return modelAndView;
